07938e1f0dae886f23ceb9a8356f4de1cf72d226
Varshitha Kumari <varshitha.kumari04@gmail.com>
2025-12-11 12:11:01 +0530

    Updated backend URL for production

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes, useParams, Link } from 'react-r
 import './App.css';
 
 // JAVA BACKEND URL
-const API_BASE_URL = "http://localhost:8080/notes"; 
+const API_BASE_URL = "https://notes-app-backend-varshu.onrender.com"; 
 
 // 1. Component: Public Note View
 const PublicNote = () => {
